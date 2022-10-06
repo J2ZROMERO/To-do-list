2c4344b8e63f898e89e3411f89991632c31d799d
Jhosep <jose-zromero@hotmail.com>
2022-10-06 13:22:04 -0500

    Add remove

--- src/index.js
+++ src/index.js
@@ -1,31 +1,49 @@
 import './style.css';
 import enter from './img/enter.png';
 import refresh from './img/refresh.png';
-import dots from './img/dots.png';
+import task from './modules/structure';
 
 document.querySelector('.refresh').src = refresh;
 document.querySelector('.enter').src = enter;
+const inputTask = document.querySelector('.addToDo');
 const contentTask = document.querySelector('.pctasks');
 
-const tasktR = [{ completed: false, description: 'go to run', index: 0 }, { completed: false, description: 'go to eat', index: 2 }, { completed: false, description: 'go to sleep', index: 3 }];
+const taskV = new task;
 
-window.addEventListener('load', () => {
-  for (let i = 0; i < tasktR.length; i += 1) {
-    let task = false;
-    if (tasktR[i].completed === true) {
-      task = ` <li class="tsk">
-      <input type="checkbox" class="checkB" checked name="vehicle1" value="Bike">
-      <label class="nametsk"> ${tasktR[i].description}</label> 
-      <img class="dot" src='${dots}' alt=""> <hr>
-             </li>`;
-    } else {
-      task = ` <li class="tsk">
-      <input type="checkbox" class="checkB" name="vehicle1" value="Bike">
-      <label class="nametsk"> ${tasktR[i].description}</label> 
-      <img class="dot" src='${dots}' alt=""> <hr>
-             </li>`;
-    }
+  document.querySelector('.enter').addEventListener('click',()=>{
+    
+    if(inputTask.value != ''){
 
-    contentTask.innerHTML += task;
+    taskV.create(contentTask,false,inputTask.value) 
+    inputTask.value = '';
+    taskV.countelement()
   }
-});
+  })
+
+contentTask.addEventListener('click',(x)=>{
+   taskV.delete(x,contentTask)
+})  
+  
+
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/modules/structure.js
+++ src/modules/structure.js
@@ -1,28 +1,82 @@
+import dots from '../img/dots.png';
+import deletei from '../img/deletei.png';
 export default class task {
-  constructor(comp, desc, ind) {
-    this.taskC = {
-      description: desc,
-      completed: comp,
-      index: ind,
-    };
+  constructor() {
+    
+
+    this.taskA = [];
+ this.count = 0;
+        
   }
 
-  createtask(dots) {
-    if (this.taskC.completed === true) {
-      this.task = ` <li class="tsk">
-        <input type="checkbox" class="checkB" checked name="vehicle1" value="Bike">
-        <label class="nametsk"> I have a bike</label> 
-        <img class="dot" src='${dots}' alt="">
-                </li>`;
-    } else {
-      this.task = ` <li class="tsk">
-        <input type="checkbox" class="checkB" name="vehicle1" value="Bike">
-        <label class="nametsk"> I have a bike</label> 
-        <img class="dot" src='${dots}' alt="">
-                </li>`;
+  create(contentTasks,comp, desc) {
+
+this.count+=1
+    /* Store array */
+  this.taskC = {};
+  this.taskC.completed = comp;
+  this.taskC.description = desc;
+  this.taskC.index = this.count;
+  this.taskA.push(this.taskC)
+/* */
+
+  let taskE = "";
+  const newArray = this.taskA.map(function(task){
+  
+  
+    if (task.completed === true) {
+    taskE = `<li class="tsk">
+      <input type="checkbox" class="checkB" checked name="vehicle1" value="Bike">
+      <input type="text" class="nametsk" value= '${task.description}' disabled> </input> 
+      <img class="dot" src='${deletei}' alt="">
+      <img class="dot" src='${dots}' alt="">
+              </li>`;
+  } else {
+    taskE = `<li html index of element js
+    class="tsk">
+    <input type="checkbox" class="checkB" name="vehicle1" value="Bike">
+    <input type="text" class="nametsk" value= '${task.description}' disabled> </input> 
+    <img class="del" src='${deletei}' alt="">
+    <img class="dot" src='${dots}' alt=""><hr>
+           </li>`;
+  }
+  
+  return taskE;
+
+})
+
+while(contentTasks.firstChild ){
+  contentTasks.removeChild(contentTasks.firstChild );
+}
+newArray.forEach((x)=>{
+  contentTasks.innerHTML += x;
+})
+
+  }
+
+countelement(){
+  console.log(this.taskA)
+  
+}
+
+  read(){
+
+  }
+
+  delete(iconD,taskParent){
+    if (iconD.target.classList == 'del') {       
+
+      taskParent.removeChild(iconD.target.parentElement);
+      
+console.log(iconD.target.classList);
+
     }
-
-    this.taskA = [this.taskC];
-    return this.task;
   }
+
+  update(){
+
+  }
+
+
+
 }
