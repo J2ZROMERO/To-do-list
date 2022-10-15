41e25c217944f5f2ca889d57c98db40fb4130304
Jhosep <jose-zromero@hotmail.com>
2022-10-14 22:04:42 -0500

    Apply linters

--- src/modules/__mocks__/structure.js
+++ src/modules/__mocks__/structure.js
@@ -1,10 +1,11 @@
 /* eslint eqeqeq: 0 */
+/* eslint-disable no-unused-expressions */
+
 export default class Task {
   constructor() {
     this.taskA = [];
     this.taskAD = [];
     this.count = 0;
-    
   }
 
   create = (contentTasks, comp, desc, ind) => {
@@ -24,63 +25,54 @@ export default class Task {
       <img class="del" src='http//img.gif' alt="">
       <img class="dot" src='http//img.gif' alt=""><hr>
              </li>`;
-      contentTasks.innerHTML += taskE;
-       this.localS(this.taskA);
-       this.getDataA()
+    contentTasks.innerHTML += taskE;
+    this.localS(this.taskA);
+    this.getDataA();
   }
 
   delete = (iconD, taskParent) => {
-  
-     taskParent.removeChild(iconD)
-     
-     this.localS(this.taskA);
-     const getls = JSON.parse(localStorage.getItem('tasks'));
+    taskParent.removeChild(iconD);
 
-      const farray = getls.filter(x => x.index != iconD.id  )
-      this.localS(farray);
-      return  taskParent
+    this.localS(this.taskA);
+    const getls = JSON.parse(localStorage.getItem('tasks'));
+
+    const farray = getls.filter((x) => x.index != iconD.id);
+    this.localS(farray);
+    return taskParent;
   }
-
 
   localS = (data) => {
     localStorage.setItem('tasks', JSON.stringify(data));
   }
 
-
   update = (x) => {
     const textinput = x.children[1];
- 
-    if ( x.children[2].className === 'check') {
-      const index = x.parentElement.getAttribute('id');
-      const inputenrties = "data changed on mock";
+
+    if (x.children[2].className === 'check') {
+      const inputenrties = 'data changed on mock';
       textinput.value = inputenrties;
-     
+
       const datamodified = this.taskA.map((x) => {
-       switch (x.index) {
-case 3:
-x.description = inputenrties;
-break;
-       }
-               
+        switch (x.index) {
+          case 3:
+            x.description = inputenrties;
+            break;
+
+          default: null;
+        }
+
         return x;
       });
-
 
       this.taskA = datamodified;
       this.localS(this.taskA);
     }
-this.getDataA()
-    return textinput.value
-   
-}
-
-
-
-
-
+    this.getDataA();
+    return textinput.value;
+  }
 
 stat = (x) => {
-  const index = x.id; 
+  const index = x.id;
 
   if (x.children[0].className === 'checkB') {
     x.children[0].checked = true;
@@ -92,19 +84,17 @@ stat = (x) => {
       return x;
     });
     this.localS(datamodified);
-  
   }
-  return x.children[0].checked
+  return x.children[0].checked;
 }
 
 /***/
-clearCompleted(x){
-
-for(let i = 0; i < x.childElementCount;i++){ 
-  if(x.children[i].id == 1 || x.children[i].id == 2 || x.children[i].id == 3){
-      x.children[i].children[0].checked = true
+clearCompleted(x) {
+  for (let i = 0; i < x.childElementCount; i += 1) {
+    if (x.children[i].id == 1 || x.children[i].id == 2 || x.children[i].id == 3) {
+      x.children[i].children[0].checked = true;
     }
-}
+  }
   const [...taskVa] = x.childNodes;
   const nvtf = taskVa.filter((x) => x.children[0].checked == false);
 
@@ -122,49 +112,31 @@ for(let i = 0; i < x.childElementCount;i++){
     });
     return '';
   });
-  this.setDataA(obnew); 
-this.getDataA()
-return x.children[0].id
-
+  this.setDataA(obnew);
+  this.getDataA();
+  return x.children[0].id;
 }
- 
- /** */
 
-
+/** */
 
  setDataA = (data) => {
-  this.taskA = data;
-  this.taskA.map((elem, index) => {
-    elem.index = (index + 1);
-    return elem;
-  });
-  this.localS(this.taskA);
-}
-
+   this.taskA = data;
+   this.taskA.map((elem, index) => {
+     elem.index = (index + 1);
+     return elem;
+   });
+   this.localS(this.taskA);
+ }
 
   localS = (data) => {
     localStorage.setItem('tasks', JSON.stringify(data));
   }
 
+  getDataA = () => JSON.parse(localStorage.getItem('tasks'))
 
-  getDataA() {
-    
-    return JSON.parse(localStorage.getItem('tasks'));
-  }
-
-  
   setDat = (val) => {
     val.forEach((x) => {
       this.taskA.push(x);
     });
   }
-
-
-
-
-
-
-
 }
-
-
--- src/modules/__tests__/structure.test.js
+++ src/modules/__tests__/structure.test.js
@@ -1,134 +1,109 @@
 /**
  * @jest-environment jsdom
  */
-jest.mock('../structure.js')
-jest.mock('../status.js')
+/* eslint eqeqeq: 0 */
+import Task from '../structure.js';
 
-import Task from '../structure.js'
+jest.mock('../structure.js');
+jest.mock('../status.js');
 
+describe('Add a li element to ul list', () => {
+  test('Add one new item to the list', () => {
+    // Arange
+    const ts = new Task();
+    const parentC = document.createElement('ul');
 
+    // act
+    parentC.setAttribute('id', 'list');
+    ts.create(parentC, 'completed', 'feed pets', 1);
 
-describe('Add a li element to ul list', ()=>{
-    
-    test('Add one new item to the list', () => {
-        //Arange
-        const ts = new Task()
-        const parentC = document.createElement('ul')
-      
-        //act
-        parentC.setAttribute("id", "list")
-        ts.create(parentC,"completed","feed pets",1)
-        
-  
-        //Assert
-        expect(parentC.childElementCount).toBe(1);
-        // console.log("Add one item on LS",ts.getDataA())
+    // Assert
+    expect(parentC.childElementCount).toBe(1);
+    // console.log("Add one item on LS",ts.getDataA())
+  });
+});
 
-      });
+describe('remove a li element to ul list', () => {
+  test('Add one new item to the list', () => {
+    // Arange
+    const ts = new Task();
+    const parentC = document.createElement('ul');
+    parentC.setAttribute('id', 'list');
 
+    // act
+    ts.create(parentC, 'completed', 'feed pets', 1);
+    ts.create(parentC, 'completed', 'work out at the gym', 2);
+    ts.create(parentC, 'completed', 'dance on the beach', 3);
 
+    for (let i = 0; i < parentC.childElementCount; i += 1) {
+      if (parentC.children[i].id == 2) {
+        ts.delete(parentC.children[i], parentC);
+      }
+    }
 
-})
+    // Assert
+    expect(parentC.childElementCount).toBe(2);
+    // console.log("Remove one item on LS",ts.getDataA())
+  });
+});
 
-describe('remove a li element to ul list', ()=>{
+describe('Update a input element of ul list', () => {
+  test('Modify the element whith id=3 and Update the input value to "data changed on mock"', () => {
+  // Arange
+    const ts = new Task();
+    const parentC = document.createElement('ul');
+    parentC.setAttribute('id', 'list');
 
-    test('Add one new item to the list', () => {
-    
-        //Arange
-        const ts = new Task()
-        const parentC = document.createElement('ul')
-        parentC.setAttribute("id", "list")
-    
-        //act
-        ts.create(parentC,"completed","feed pets",1)
-        ts.create(parentC,"completed","work out at the gym",2)
-        ts.create(parentC,"completed","dance on the beach",3)
-        
-        for(let i = 0 ; i < parentC.childElementCount;i++){
-        if(parentC.children[i].id == 2){
-        ts.delete(parentC.children[i],parentC)
-         }}
+    // act
+    ts.create(parentC, false, 'feed pets', 1);
+    ts.create(parentC, false, 'work out at the gym', 2);
+    ts.create(parentC, false, 'dance on the beach', 3);
 
-        //Assert
-        expect(parentC.childElementCount).toBe(2);
-        // console.log("Remove one item on LS",ts.getDataA())
-    
-      });
-    
+    const newinput = ts.update(parentC.children[2]);
 
+    // Assert
+    expect(newinput).toMatch('data changed on mock');
+    // console.log("update input element id=3 on LS",ts.getDataA())
+  });
+});
 
+describe('check if a task is checked', () => {
+  test('Modify the element whith id=1 if checked = true"', () => {
+  // Arange
+    const ts = new Task();
+    const parentC = document.createElement('ul');
+    parentC.setAttribute('id', 'list');
 
-  })
+    // act
+    ts.create(parentC, false, 'feed pets', 1);
+    ts.create(parentC, false, 'work out at the gym', 2);
+    ts.create(parentC, false, 'dance on the beach', 3);
 
-  
-  describe('Update a input element of ul list', ()=>{
+    const result = ts.stat(parentC.children[0]);
 
-    test('Modify the element whith id=3 and Update the input value to "data changed on mock"', () => {
-  //Arange
-  const ts = new Task()
-  const parentC = document.createElement('ul')
-  parentC.setAttribute("id", "list")
+    // Assert
+    expect(result).toBe(true);
+    // console.log("checked on id=1 true on LS" ,ts.getDataA())
+  });
+});
 
-  //act
-  ts.create(parentC,false,"feed pets",1)
-  ts.create(parentC,false,"work out at the gym",2)
-  ts.create(parentC,false,"dance on the beach",3)
-  
-  const newinput =   ts.update(parentC.children[2])
- 
-  //Assert
-        expect(newinput).toMatch("data changed on mock");
-        //console.log("update input element id=3 on LS",ts.getDataA())
-    });
-  })
+describe('Clear all checked tasks', () => {
+  test('there are 4 tasks 3 of them are checked and are going to remove,task 4 must exist"', () => {
+  // Arange
+    const ts = new Task();
+    const parentC = document.createElement('ul');
+    parentC.setAttribute('id', 'list');
 
+    // act
+    ts.create(parentC, false, 'feed pets', 1);
+    ts.create(parentC, false, 'work out at the gym', 2);
+    ts.create(parentC, false, 'dance on the beach', 3);
+    ts.create(parentC, false, 'date with friends', 4);
 
+    const result = ts.clearCompleted(parentC);
 
-
-  describe('check if a task is checked', ()=>{
-
-    test('Modify the element whith id=1 if checked = true"', () => {
-  //Arange
-  const ts = new Task()
-  const parentC = document.createElement('ul')
-  parentC.setAttribute("id", "list")
-
-  //act
-  ts.create(parentC,false,"feed pets",1)
-  ts.create(parentC,false,"work out at the gym",2)
-  ts.create(parentC,false,"dance on the beach",3)
-  
-  const result =  ts.stat(parentC.children[0])
-
-        //Assert
-        expect(result).toBe(true);
-        //console.log("checked on id=1 true on LS" ,ts.getDataA())
-    });
-  })
-
-
-
-
-
-
-  describe('Clear all checked tasks', ()=>{
-
-    test('there are 4 tasks 3 of them are checked and are going to remove,task 4 must exist"', () => {
-  //Arange
-  const ts = new Task()
-  const parentC = document.createElement('ul')
-  parentC.setAttribute("id", "list")
-
-  //act
-  ts.create(parentC,false,"feed pets",1)
-  ts.create(parentC,false,"work out at the gym",2)
-  ts.create(parentC,false,"dance on the beach",3)
-  ts.create(parentC,false,"date with friends",4)
-  
-  const result = ts.clearCompleted(parentC)
-
-        //Assert
-        expect(parseInt(result)).toBe(4);
-        //console.log("Remove  1,2,3 tasks on LS, id=4 was 'data with friends'",ts.getDataA())
-    });
-  })
+    // Assert
+    expect(parseInt(result, 10)).toBe(4);
+    // console.log("Remove  1,2,3 tasks on LS, id=4 was 'data with friends'",ts.getDataA())
+  });
+});
